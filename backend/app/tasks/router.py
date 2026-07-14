84b8be8aedfd393df0b041ba35e1b7595d3e5a4d
Leon Biju <leonbiju25@gmail.com>
2026-07-14 12:03:40 +0100

    add basic endpoints and stubs for other crud endpoints

--- backend/app/tasks/router.py
+++ backend/app/tasks/router.py
@@ -8,43 +8,57 @@ from app.core.deps import get_current_user, get_db
 
 from app.users.models import User
 from app.tasks.schemas import TaskCreate, TaskRead, TaskUpdate
-from app.tasks.services import create_task, update_task, TaskNotFoundException, get_user_tasks
+from app.tasks.exceptions import TaskNotFoundException
+import app.tasks.services as task_services
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
-@router.post("/create")
-async def create(
+@router.post("/")
+async def create_task(
     payload: TaskCreate,
     session: AsyncSession = Depends(get_db),
     user: User = Depends(get_current_user)
 ) -> TaskRead:
     
-    task = await create_task(session, payload, user.id)
+    task = await task_services.create(session, payload, user.id)
     
     return task
 
+@router.get("/")
+async def list_tasks(
+    
+) -> List[TaskRead]:
+    pass
 
-@router.patch("/update/{task_id}")
-async def update(
+
+@router.get("/{task_id}")
+async def get_task(
+    task_id: uuid.UUID,
+    session: AsyncSession = Depends(get_db),
+    user: User = Depends(get_current_user)
+) -> TaskRead:
+    tasks = await task_services.get(session, task_id, user.id)
+
+    return tasks
+
+
+@router.patch("/{task_id}")
+async def update_task(
     task_id: uuid.UUID,
     payload: TaskUpdate,
     session: AsyncSession = Depends(get_db),
     user: User = Depends(get_current_user)
 ) -> TaskRead:
     try:
-        task = await update_task(session, payload, task_id, user.id)
+        task = await task_services.update_task(session, payload, task_id, user.id)
     except TaskNotFoundException:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Task not found.")
     
     return task
 
 
-# This is a temporary and should be replaced with a nice well oiled filter type
-@router.get("/my_tasks")
-async def gettasks(
-    session: AsyncSession = Depends(get_db),
-    user: User = Depends(get_current_user)
-) -> List[TaskRead]:
-    tasks = await get_user_tasks(session, user.id)
+@router.delete("/{task_id}")
+async def delete_task():
+    pass
 
-    return tasks
+
--- backend/app/tasks/services.py
+++ backend/app/tasks/services.py
@@ -6,8 +6,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.tasks.schemas import TaskCreate, TaskUpdate
 from app.tasks.models import Task
+from app.tasks.exceptions import TaskNotFoundException
 
-async def create_task(
+async def create(
     session: AsyncSession,
     task_create: TaskCreate, 
     user_id: uuid.UUID,
@@ -23,37 +24,52 @@ async def create_task(
 
     return task
 
-class TaskNotFoundException(Exception):
+
+async def get(
+    session: AsyncSession,
+    task_id: uuid.UUID, 
+    user_id: uuid.UUID,
+) -> Task:
+    result = await session.execute(
+        select(Task)
+        .where(Task.id == task_id and Task.user_id == user_id)
+    )
+
+    task = result.scalar_one_or_none()
+    if task is None:
+        raise TaskNotFoundException()
+    
+    return task
+
+
+async def filter_by() -> List[Task]:
     pass
 
-async def update_task(
+
+async def update(
     session: AsyncSession,
     payload: TaskUpdate,
     task_id: uuid.UUID,
     user_id: uuid.UUID,
 ) -> Task:
     
-    result = await session.execute(
-        select(Task)
-        .where(Task.id == task_id and Task.user_id == user_id)
-    )
-    task = result.scalar_one_or_none()
+    task = get(session, task_id, user_id)
 
-    if task is None:
-        raise TaskNotFoundException()
-    
     task_updates = payload.model_dump(exclude_unset=True)
-
     for key, val in task_updates.items():
         setattr(task, key, val)
 
     await session.commit()
     await session.refresh(task)
 
-
     return task
 
 
+async def delete() -> Task:
+    pass
+
+
+#lol get rid of this replace with filter_by
 async def get_user_tasks(
     session: AsyncSession,
     user_id: uuid.UUID
