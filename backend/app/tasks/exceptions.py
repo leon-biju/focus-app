4810e0b60767d0a10c3807ceb0f64d9efe13e32d
Leon Biju <leonbiju25@gmail.com>
2026-07-22 16:59:35 +0100

    backend: refactor task exceptions to inherit from AppException, we get auto exception -> http error handling

--- backend/app/tasks/exceptions.py
+++ backend/app/tasks/exceptions.py
@@ -1,2 +1,9 @@
-class TaskNotFoundException(Exception):
-    pass
+from app.core.exceptions import AppException
+
+class TaskNotFoundException(AppException):
+    status_code = 404
+    detail = "Task not found."
+    
+class TaskConflictException(AppException):
+    status_code = 409
+    detail = "Task was modified by another request. Try again."
